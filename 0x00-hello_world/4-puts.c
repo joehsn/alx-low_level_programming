9f61989138097a619ca31da42f0aa11c37b1bc97
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-03-16 09:02:32 -0700

    F___ betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main (void){
+/*
+ * main - Entry point
+ * Description: 'a program that prints exactly "Programming is like..'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
