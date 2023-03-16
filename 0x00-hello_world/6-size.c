8b10328fabf62b1dcd89efba2128d9b57134fce0
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-03-16 10:39:22 -0700

    hope it works for puts task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	long int li;
+	long long int lli;
+	char c;
+	float f;
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
