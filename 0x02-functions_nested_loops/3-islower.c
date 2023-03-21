12510b3e73fb31cdd044d73f424d0c8aa5c5f9fc
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-03-21 11:41:26 -0700

    changed to lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,16 @@
 /**
- * _isLower - checks if a character is lowercase
+ * _islower - checks if a character is lowercase
  * @c: The character to check
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
 
+#include <ctype.h>
 #include "main.h"
 
-int _isLower(int c)
+int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (islower(c))
 	{
 		return (1);
 	}
