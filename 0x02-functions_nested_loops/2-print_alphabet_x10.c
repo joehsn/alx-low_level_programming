efa6efc0dab3f20cc2f032a59729d5237ae127b7
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-03-21 10:34:29 -0700

    varibles declerad alone

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,12 @@
 
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (char c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
