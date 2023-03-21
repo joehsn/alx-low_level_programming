58b640f5d7d1458dc63fedbcf53ce8c07cacc7ea
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-03-21 11:24:56 -0700

    stupid lowrcase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,10 @@
 /**
- * _islower - checks if a character is lowercase
+ * _isLower - checks if a character is lowercase
  * @c: The character to check
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
+
 #include "main.h"
 
 int _isLower(int c)
