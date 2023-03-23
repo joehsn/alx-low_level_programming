a82d4b41ff5164bba3b6011a9020f6e5e3a19108
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-03-23 06:03:33 -0700

    curly braces added

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,11 +13,19 @@ void print_diagonal(int n)
 	int y, x;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
+	{
 		for (y = 0; y < n; y++)
+		{
 			for (x = 0; x < y; x++)
+			{
 				_putchar(' ');
+			}
 		_putchar('\\');
 		_putchar('\n');
+		}
+	}
 }
