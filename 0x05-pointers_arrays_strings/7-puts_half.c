fdb3f538c3f76fc9a296c4fcd52ac78b413b9b13
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-03-27 12:33:33 -0700

    a function that prints a string, in reverse

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,9 +11,9 @@
 
 void puts_half(char *str)
 {
-	int i, len = strlen(str);
+	int i, len = strlen(str), n = (len % 2 == 0) ? len / 2 : (len - 1) / 2;
 
-	for (i = (len - 1) / 2; i < len; i++)
+	for (i = n; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
