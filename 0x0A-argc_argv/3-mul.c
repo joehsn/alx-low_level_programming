5e02f2e490002710db4cff2a3883c077a1cbfd8f
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-04-09 13:21:23 -0700

    naughty betty ;)

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,18 +13,19 @@
 
 int main(int argc, char *argv[])
 {
-  int num1, num2, res;
+	int num1, num2, res;
 
-  if (argc != 3) {
-    printf("Error\n");
-    return 1;
-  }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-  num1 = atoi(argv[1]);
-  num2 = atoi(argv[2]);
-  res = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	res = (num1 * num2);
 
-  printf("%d\n", res);
-  return 0;
+	printf("%d\n", res);
+	return (0);
 }
 
