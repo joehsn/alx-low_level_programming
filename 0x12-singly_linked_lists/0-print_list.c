f124b64b2cae75a4a2160dd52fd911a7e88f41fd
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-04-27 09:42:32 -0700

    1st task enahnced

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-	const list_t *current = h;
+	size_t nodes = 0;
 
-	while (current != NULL)
+	while (h)
 	{
-		if (current->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", current->len, current->str);
 
-		current = current->next;
-		count++;
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
 	}
 
-	return (count);
+	return (nodes);
 }
-
