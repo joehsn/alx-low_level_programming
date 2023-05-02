8b5d7147754cd2cadc4301eae48d233f9a290141
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-05-02 10:57:01 -0700

    a program that multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 98 on error
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 == 0 || num2 == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	product = num1 * num2;
+	printf("%d\n", product);
+
+	return (0);
+}
+
