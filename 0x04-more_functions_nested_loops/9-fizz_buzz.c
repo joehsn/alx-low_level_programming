5bfd5b654249012fbfb408f25515308fa61e3bab
joehsn <youssef.hassan.mahmoud@gmail.com>
2023-09-14 14:56:54 -0700

    fuzzbuzz for the 4th time

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,13 +19,17 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if (i + 1 == 100)
 		{
-			printf("Buzz ");
+			printf("%d", i);
 		}
 		else
 		{
