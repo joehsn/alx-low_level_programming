a55621c78264fe3d1a433d849d3be2123579e588
joehsn <youssef.hassan.mahmoud@gmail.com>
2023-09-19 13:45:52 -0700

    IT'S SUPPOSED TO BE '+' INSTEAD OF '-'.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@
 
 void puts_half(char *str)
 {
-	int i, len = strlen(str), n = (len % 2 == 0) ? len / 2 : (len - 1) / 2;
+	int i, len = strlen(str), n = (len % 2 == 0) ? len / 2 : (len + 1) / 2;
 
 	for (i = n; i < len; i++)
 	{
