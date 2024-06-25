7d9762282301bb66d6f3b769e08d531276d12cdc
joehsn <joehsn@outlook.com>
2024-06-25 22:16:37 +0300

    1. memcpy task is done

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,6 +16,16 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest, src, n);
+  unsigned int i;
+
+  if (!n)
+  {
+    n = strlen(src);
+  }
+
+  for (i = 0; i < n; i++)
+  {
+    dest[i] = src[i];
+  }
 	return (dest);
 }
