d88e22e53feb9179eaba147ff37ac1a333dfac85
joehsn <89865074+joehsn@users.noreply.github.com>
2025-06-25 01:35:42 +0300

    The World looks like a multiplication-table...

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,7 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
+int count_digits(const int n);
 /**
  * print_times_table - a function that prints
  * the n times table, starting with 0.
@@ -10,6 +11,8 @@
 
 void print_times_table(int n)
 {
+	int total = n * n;
+	int total_digits = count_digits(total);
 	int row, col;
 
 	if (n > 15 || n < 0)
@@ -20,31 +23,85 @@ void print_times_table(int n)
 		for (col = 0; col <= n; col++)
 		{
 			int res = row * col;
+			int res_digits = count_digits(res);
+			int *rev_res_digits = (int *)malloc(sizeof(int) * res_digits);
+			int spaces = total_digits - res_digits;
+			int i;
 
-			if (res >= 100)
+			if (res == 0)
 			{
-				_putchar('0' + (res / 100 % 10));
-				_putchar('0' + (res / 10 % 10));
+				if (col > 0)
+				{
+					while (spaces)
+					{
+						_putchar(' ');
+						spaces--;
+					}
+					_putchar('0');
+				}
+				else
+				{
+					_putchar('0');
+				}
 			}
-			else if (res >= 10)
+			else
 			{
-				_putchar(' ');
-				_putchar('0' + (res / 10 % 10));
-			}
-			else if (res < 10 && col != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
+				for (i = res_digits - 1; i >= 0; i--)
+				{
+					rev_res_digits[i] = res % 10;
+					res /= 10;
+				}
+
+				i = 0;
+
+				while (spaces)
+				{
+					_putchar(' ');
+					spaces--;
+				}
+
+				for (i = 0; i < res_digits; i++)
+				{
+					_putchar(rev_res_digits[i] + '0');
+				}
 			}
 
-			_putchar('0' + (res % 10));
+			free(rev_res_digits);
 
-			if (!(col == n))
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			if (col == n)
+				continue;
+
+			_putchar(',');
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
+}
+
+/**
+ * count_digits - a function that counts the n digits.
+ * @n: the given number
+ * Return: int
+ */
+int count_digits(const int n)
+{
+	int t = n;
+	int no_digits = 0;
+
+	if (t == 0)
+		return (1);
+
+	if (t < 0)
+	{
+		no_digits++;
+		t = -t;
+	}
+
+	while (t)
+	{
+		no_digits++;
+		t /= 10;
+	}
+
+	return (no_digits);
 }
