3663df2b1d347bc1fc976b76f6e5d5ecad316492
joehsn <89865074+joehsn@users.noreply.github.com>
2025-07-09 14:33:14 +0300

    2. Dreams feel real while we're in them...

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strlen_recursion - a function that returns the length of a string
@@ -9,7 +8,13 @@
  * Return: intger
  */
 
-int _strlen_recursion(char *s)
-{
-	return (strlen(s));
+int _strlen_recursion(char *s) {
+	int len = 0;
+	if (*s == '\0')
+		return (0);
+
+	len++;
+	s++;
+
+	return (len + _strlen_recursion(s));
 }
