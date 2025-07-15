33ea82b1c4f4634342de979b653e659a2db4b717
joehsn <89865074+joehsn@users.noreply.github.com>
2025-07-15 20:20:14 +0300

    0. A library is not a luxury but one of the necessities of life... x2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,7 +10,8 @@
  * Return: intger
  */
 
-int main(__attribute__((unused)) int argc, char *argv[]) {
-  printf("%s\n", argv[0]);
-  return (0);
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
 }
