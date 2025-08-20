51fd50ec4c1726d104ee68a884d44b846f218463
joehsn <89865074+joehsn@users.noreply.github.com>
2025-08-20 15:42:18 +0300

    5. The Hare and the Tortoise... what a betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * premain - A function that runs before the main function is executed
+ *
+ * Returns: a void
+ */
 void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf(
+			"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+	      );
 }
 
