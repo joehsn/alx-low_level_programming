3423737ab1425429ae7d2d1d9feba975754cedbb
joehsn <89865074+joehsn@users.noreply.github.com>
2025-08-22 13:59:40 +0300

    1. 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints
+ * the binary representation of a number.
+ *
+ * @n: decimal numbere
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n / 2);
+	_putchar((n % 2) + '0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,9 @@
 
 #include <stdlib.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 int _pow(const int base, const int to_power);
+void print_binary(unsigned long int n);
 
 #endif /* !MAIN_H */
